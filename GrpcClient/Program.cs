06a6919b1810cde235db057d44104eb5ffd54e0f
F1nnD3v <oscrafters87@gmail.com>
2022-05-09 00:30:41 +0100

    first commit

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,69 @@
+﻿using Grpc.Net.Client;
+using GrpcServer;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcClient
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //Console.WriteLine("Escreva o seu nome: ");
+            //string nome = Console.ReadLine();
+            //var input = new HelloRequest { Name = nome };
+            //var client = new Greeter.GreeterClient(channel);
+
+            //var reply = await client.SayHelloAsync(input);
+
+            //Console.WriteLine(reply.Message);
+
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Cliente.ClienteClient(channel);
+            bool sair = false;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("---|Menu Clientes|---");
+                Console.WriteLine("1 - Pedir info cliente");
+                Console.WriteLine("2 - Sair");
+                int opcao = Convert.ToInt32(Console.ReadLine());
+                if(opcao == 1)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Escreva o id do cliente que deseja ver as informações: ");
+                    int idPedido = Convert.ToInt32(Console.ReadLine());
+                    Console.Clear();
+                    var idRequested = new ClienteLookupModel { IdCliente = idPedido };
+                    var cliente = await client.getClienteInfoAsync(idRequested);
+                    Console.WriteLine("Primeiro nome: " + cliente.PrimeiroNome);
+                    Console.WriteLine("Último nome: " + cliente.UltimoNome);
+                    Console.WriteLine("Idade: " + cliente.Idade);
+                    Console.ReadLine();
+
+                }else if(opcao == 2)
+                {
+                    Console.WriteLine("Tem mesmo a certeza que deseja sair?S/N");
+                    string opcaoSair = Console.ReadLine().ToLower();
+                    if(opcaoSair == "s")
+                    {
+                        sair = true;
+                    }else if(opcaoSair == "n")
+                    {
+                        sair=false;
+                    }
+                    else
+                    {
+                        while(opcaoSair != "s" && opcaoSair != "n")
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Opção inválida!");
+                            Console.WriteLine("Tem mesmo a certeza que deseja sair?S/N");
+                            opcaoSair = Console.ReadLine().ToLower();
+                        }
+                    }
+                }
+            }while (sair == false);
+        }
+    }
+}
---
+++ GrpcServer/Services/ClientesService.cs
@@ -0,0 +1,41 @@
+﻿using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace GrpcServer.Services
+{
+    public class ClientesService : Cliente.ClienteBase
+    {
+        private readonly ILogger<ClientesService> _logger;
+
+        public ClientesService(ILogger<ClientesService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<ClienteModel> getClienteInfo(ClienteLookupModel request, ServerCallContext context)
+        {
+            ClienteModel output = new ClienteModel();
+
+            if(request.IdCliente == 1)
+            {
+                output.PrimeiroNome = "Ricardo";
+                output.UltimoNome = "Silva";
+                output.Idade = 18;
+            }else if(request.IdCliente == 2)
+            {
+                output.PrimeiroNome = "João";
+                output.UltimoNome = "Damasio";
+                output.Idade = 21;
+            }
+            else
+            {
+                output.PrimeiroNome = "Maria";
+                output.UltimoNome = "Eduarda";
+                output.Idade = 23;
+            }
+
+            return Task.FromResult(output);
+        }
+    }
+}
